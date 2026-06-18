cd3b01f76b585ff8c4bb8eddd09292af8c01ebc7
leaf <145163317+astesicelive@users.noreply.github.com>
2026-06-19 01:49:14 +0200

    Update animeverse.js

--- child/plot/animeverse.js
+++ child/plot/animeverse.js
@@ -90,4 +90,35 @@ var animeverse = [
       }
     ],
   }
-]
+];
+
+function generate_timeline() {
+
+  animeverse.forEach((day) => {
+    let current = document.createElement('div')
+    current.setAttribute('class', 'container');
+
+    for (let i = 0; i < day.content.length; i++) {
+      let section = day.content[i];
+      let list = [];
+      section.content.forEach((li) => {
+        list.push(`<li>${li}</li>`);
+      });
+
+      if (day.content.length == 1) {
+        current.innerHTML(`<ul>${list.join('')}</ul>`)
+      } else {
+        var choice = document.createElement('div')
+        choice.setAttribute('class', 'choice')
+        choice.innerHTML(`<ul>${list.join('')}</ul>`)
+        current.appendChild(choice)
+      };
+    };
+
+    document.body.appendChild(current)
+
+  });
+  
+};
+
+generate_timeline();
