e9941da5ffa61842eaa16635fcb4bc302b1b446f
leaf <145163317+astesicelive@users.noreply.github.com>
2026-06-19 01:03:26 +0200

    Update generate.js

--- child/js/generate.js
+++ child/js/generate.js
@@ -2,9 +2,30 @@ function generate_timeline() {
   var body = document.getElementsByTagName('body')
 
   animeverse.forEach((day) => {
-    var current = createElement('div').setAttribute('class', 'container')
+    var current = createElement('div').setAttribute('class', 'container');
+
+    for (let i = 0; i < day.content.length; i++) {
+      let section = day.content[i];
+      let list = [];
+      section.content.forEach((li) => {
+        list.push(`<li>${li}</li>`);
+      });
+
+      if (day.content.length == 1) {
+        
+      } else {}
+    };
+
+    day.content.forEach((section) => {
+      var list = [];
+      section.content.forEach((li) => {
+        list.push(`<li>${li}</li>`)
+      });
+    });
     if (day.content.length > 1) {
-      
+      var choice = createElement('div').setAttribute('class', 'choice')
+
+      var list = []
     }
   })
   
