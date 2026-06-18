86573271ac86cc2b3427c4ecfb68c38498999427
leaf <145163317+astesicelive@users.noreply.github.com>
2026-06-19 00:29:31 +0200

    Create js/generate.js

---
+++ child/js/generate.js
@@ -0,0 +1,8 @@
+function generate_timeline() {
+  var body = document.getElementsByTagName('body')
+
+  animeverse.forEach((day) => {
+    var current = createElement('div').setAttribute('class', 'container')
+  })
+  
+}
