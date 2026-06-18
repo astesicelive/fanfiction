b1ba65fed452cf8c63d8282551693a8880be2c98
leaf <145163317+astesicelive@users.noreply.github.com>
2026-06-18 23:30:29 +0200

    Create animeverse.js

---
+++ child/plot/animeverse.js
@@ -0,0 +1,44 @@
+var animeverse = [
+  {
+    'title': 'Sunday',
+    'content': [
+      'sunday content',
+    ],
+  },
+  {
+    'title': 'Monday',
+    'content': [
+      'monday content',
+    ],
+  },
+  {
+    'title': 'Tuesday',
+    'content': [
+      'tuesday content',
+    ],
+  },
+  {
+    'title': 'Wednesday',
+    'content': [
+      'wednesday content',
+    ],
+  },
+  {
+    'title': 'Thursday',
+    'content': [
+      'thursday content',
+    ],
+  },
+  {
+    'title': 'Friday',
+    'content': [
+      'friday content',
+    ],
+  },
+  {
+    'title': 'Saturday',
+    'content': [
+      'saturday content',
+    ],
+  },
+]
