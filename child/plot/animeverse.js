03605c4052161adb356031a342e072ec0c5162d0
leaf <145163317+astesicelive@users.noreply.github.com>
2026-06-19 00:32:56 +0200

    Update animeverse.js

--- child/plot/animeverse.js
+++ child/plot/animeverse.js
@@ -1,93 +1,93 @@
 var animeverse = [
   {
     'title': 'Sunday',
-    'content': {
-      1: {
+    'content': [
+      {
         'title': 'desu2',
         'content': 'desu2 sunday',
       },
-      2: {
+      {
         'title': 'p5',
         'content': 'p5 sunday',
       },
-    },
+    ],
   },
   {
     'title': 'Monday',
-    'content': {
-      1: {
+    'content': [
+      {
         'title': 'desu2',
         'content': 'desu2 monday',
       },
-      2: {
+      {
         'title': 'p5',
         'content': 'p5 monday',
       },
-    },
+    ],
   },
   {
     'title': 'Tuesday',
-    'content': {
-      1: {
+    'content': [
+      {
         'title': 'desu2',
         'content': 'desu2 tuesday',
       },
-      2: {
+      {
         'title': 'p5',
         'content': 'p5 tuesday',
       },
-    },
+    ],
   },
   {
     'title': 'Wednesday',
-    'content': {
-      1: {
+    'content': [
+      {
         'title': 'desu2',
         'content': 'desu2 wednesday',
       },
-      2: {
+      {
         'title': 'p5',
         'content': 'p5 wednesday',
       },
-    },
+    ],
   },
   {
     'title': 'Thursday',
-    'content': {
-      1: {
+    'content': [
+      {
         'title': 'desu2',
         'content': 'desu2 thursday',
       },
-      2: {
+      {
         'title': 'p5',
         'content': 'p5 thursday',
       },
-    },
+    ],
   },
   {
     'title': 'Friday',
-    'content': {
-      1: {
+    'content': [
+      {
         'title': 'desu2',
         'content': 'desu2 friday',
       },
-      2: {
+      {
         'title': 'p5',
         'content': 'p5 friday',
       },
-    },
+    ],
   },
   {
     'title': 'Saturday',
-    'content': {
-      1: {
+    'content': [
+      {
         'title': 'desu2',
         'content': 'desu2 saturday',
       },
-      2: {
+      {
         'title': 'p5',
         'content': 'p5 saturday',
       },
-    },
+    ],
   },
 ]
