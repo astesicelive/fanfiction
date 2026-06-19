2aef226c9351d984e21ebed14c6bea52fc9074f6
leaf <145163317+astesicelive@users.noreply.github.com>
2026-06-19 04:39:45 +0200

    Update generate.js

--- child/js/generate.js
+++ child/js/generate.js
@@ -1,97 +1,3 @@
-/*var animeverse = [
-  {
-    'title': 'Sunday',
-    'content': [
-      {
-        'title': 'desu2',
-        'content': ['desu2 sunday'],
-      },
-      {
-        'title': 'p5',
-        'content': ['p5 sunday'],
-      }
-    ],
-  },
-  {
-    'title': 'Monday',
-    'content': [
-      {
-        'title': 'desu2',
-        'content': ['desu2 monday'],
-      },
-      {
-        'title': 'p5',
-        'content': ['p5 monday'],
-      }
-    ],
-  },
-  {
-    'title': 'Tuesday',
-    'content': [
-      {
-        'title': 'desu2',
-        'content': ['desu2 tuesday'],
-      },
-      {
-        'title': 'p5',
-        'content': ['p5 tuesday'],
-      }
-    ],
-  },
-  {
-    'title': 'Wednesday',
-    'content': [
-      {
-        'title': 'desu2',
-        'content': ['desu2 wednesday'],
-      },
-      {
-        'title': 'p5',
-        'content': ['p5 wednesday'],
-      }
-    ],
-  },
-  {
-    'title': 'Thursday',
-    'content': [
-      {
-        'title': 'desu2',
-        'content': ['desu2 thursday'],
-      },
-      {
-        'title': 'p5',
-        'content': ['p5 thursday'],
-      }
-    ],
-  },
-  {
-    'title': 'Friday',
-    'content': [
-      {
-        'title': 'desu2',
-        'content': ['desu2 friday'],
-      },
-      {
-        'title': 'p5',
-        'content': ['p5 friday'],
-      }
-    ],
-  },
-  {
-    'title': 'Saturday',
-    'content': [
-      {
-        'title': 'desu2',
-        'content': ['desu2 saturday'],
-      },
-      {
-        'title': 'p5',
-        'content': ['p5 saturday'],
-      }
-    ],
-  }
-];*/
-
 function generate_timeline() {
 
   animeverse.forEach((day) => {
@@ -106,14 +12,14 @@ function generate_timeline() {
       } else {
         list = `${i}`;
       }
-      let result = document.createTextNode(list);
+
 
       if (day.content.length == 1) {
-        current.appendChild(result);
+        current.innerHTML(list);
       } else {
         var choice = document.createElement('div');
         choice.setAttribute('class', 'choice');
-        choice.appendChild(result);
+        choice.innerHTML(list);
         current.appendChild(choice);
       };
     };
