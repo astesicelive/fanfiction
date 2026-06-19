8fdd467443336eefa6c884dcfbd9ec06cbaf06cf
leaf <145163317+astesicelive@users.noreply.github.com>
2026-06-19 06:24:00 +0200

    Update generate.js

--- child/js/generate.js
+++ child/js/generate.js
@@ -1,3 +1,97 @@
+const animeverse = [
+  {
+    'title': 'Sunday',
+    'content': [
+      {
+        'title': 'desu2',
+        'content': ['desu2 sunday'],
+      },
+      {
+        'title': 'p5',
+        'content': ['p5 sunday'],
+      }
+    ],
+  },
+  {
+    'title': 'Monday',
+    'content': [
+      {
+        'title': 'desu2',
+        'content': ['desu2 monday'],
+      },
+      {
+        'title': 'p5',
+        'content': ['p5 monday'],
+      }
+    ],
+  },
+  {
+    'title': 'Tuesday',
+    'content': [
+      {
+        'title': 'desu2',
+        'content': ['desu2 tuesday'],
+      },
+      {
+        'title': 'p5',
+        'content': ['p5 tuesday'],
+      }
+    ],
+  },
+  {
+    'title': 'Wednesday',
+    'content': [
+      {
+        'title': 'desu2',
+        'content': ['desu2 wednesday'],
+      },
+      {
+        'title': 'p5',
+        'content': ['p5 wednesday'],
+      }
+    ],
+  },
+  {
+    'title': 'Thursday',
+    'content': [
+      {
+        'title': 'desu2',
+        'content': ['desu2 thursday'],
+      },
+      {
+        'title': 'p5',
+        'content': ['p5 thursday'],
+      }
+    ],
+  },
+  {
+    'title': 'Friday',
+    'content': [
+      {
+        'title': 'desu2',
+        'content': ['desu2 friday'],
+      },
+      {
+        'title': 'p5',
+        'content': ['p5 friday'],
+      }
+    ],
+  },
+  {
+    'title': 'Saturday',
+    'content': [
+      {
+        'title': 'desu2',
+        'content': ['desu2 saturday'],
+      },
+      {
+        'title': 'p5',
+        'content': ['p5 saturday'],
+      }
+    ],
+  }
+];
+
 function generate_timeline(v) {
 
   let txt = document.createTextNode(v);
@@ -46,4 +140,4 @@ function generate_timeline(v) {
   
 };
 
-generate_timeline('1.0.2.4')
+generate_timeline('1.0.2.5')
