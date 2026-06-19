153a3331af833f9f2572901d07c658d05342bce6
leaf <145163317+astesicelive@users.noreply.github.com>
2026-06-19 15:48:25 +0200

    Update animeverse.js

--- child/plot/animeverse.js
+++ child/plot/animeverse.js
@@ -5,10 +5,6 @@ var animeverse = [
       {
         'title': 'desu2',
         'content': ['desu2 sunday'],
-      },
-      {
-        'title': 'p5',
-        'content': ['p5 sunday'],
       }
     ],
   },
@@ -18,10 +14,6 @@ var animeverse = [
       {
         'title': 'desu2',
         'content': ['desu2 monday'],
-      },
-      {
-        'title': 'p5',
-        'content': ['p5 monday'],
       }
     ],
   },
@@ -31,10 +23,6 @@ var animeverse = [
       {
         'title': 'desu2',
         'content': ['desu2 tuesday'],
-      },
-      {
-        'title': 'p5',
-        'content': ['p5 tuesday'],
       }
     ],
   },
@@ -44,10 +32,6 @@ var animeverse = [
       {
         'title': 'desu2',
         'content': ['desu2 wednesday'],
-      },
-      {
-        'title': 'p5',
-        'content': ['p5 wednesday'],
       }
     ],
   },
@@ -57,10 +41,6 @@ var animeverse = [
       {
         'title': 'desu2',
         'content': ['desu2 thursday'],
-      },
-      {
-        'title': 'p5',
-        'content': ['p5 thursday'],
       }
     ],
   },
@@ -70,10 +50,6 @@ var animeverse = [
       {
         'title': 'desu2',
         'content': ['desu2 friday'],
-      },
-      {
-        'title': 'p5',
-        'content': ['p5 friday'],
       }
     ],
   },
@@ -83,10 +59,6 @@ var animeverse = [
       {
         'title': 'desu2',
         'content': ['desu2 saturday'],
-      },
-      {
-        'title': 'p5',
-        'content': ['p5 saturday'],
       }
     ],
   }
