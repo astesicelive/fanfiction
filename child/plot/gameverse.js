40d05f331ac85f683c949206878d178cb8ef057c
leaf <145163317+astesicelive@users.noreply.github.com>
2026-06-20 12:02:03 +0200

    Update gameverse.js

--- child/plot/gameverse.js
+++ child/plot/gameverse.js
@@ -5,98 +5,98 @@ var gameverse = [
   },
   {
     'type': 'title',
-    'title': 'Sunday',
+    'class': 'Sunday',
     'content': [
       {
-        'title': 'desu2',
+        'class': 'desu2',
         'content': ['desu2 sunday'],
       },
       {
-        'title': 'p5',
+        'class': 'p5',
         'content': ['p5 sunday'],
       }
     ],
   },
   {
     'type': 'title',
-    'title': 'Monday',
+    'class': 'Monday',
     'content': [
       {
-        'title': 'desu2',
+        'class': 'desu2',
         'content': ['desu2 monday'],
       },
       {
-        'title': 'p5',
+        'class': 'p5',
         'content': ['p5 monday'],
       }
     ],
   },
   {
     'type': 'title',
-    'title': 'Tuesday',
+    'class': 'Tuesday',
     'content': [
       {
-        'title': 'desu2',
+        'class': 'desu2',
         'content': ['desu2 tuesday'],
       },
       {
-        'title': 'p5',
+        'class': 'p5',
         'content': ['p5 tuesday'],
       }
     ],
   },
   {
     'type': 'title',
-    'title': 'Wednesday',
+    'class': 'Wednesday',
     'content': [
       {
-        'title': 'desu2',
+        'class': 'desu2',
         'content': ['desu2 wednesday'],
       },
       {
-        'title': 'p5',
+        'class': 'p5',
         'content': ['p5 wednesday'],
       }
     ],
   },
   {
     'type': 'title',
-    'title': 'Thursday',
+    'class': 'Thursday',
     'content': [
       {
-        'title': 'desu2',
+        'class': 'desu2',
         'content': ['desu2 thursday'],
       },
       {
-        'title': 'p5',
+        'class': 'p5',
         'content': ['p5 thursday'],
       }
     ],
   },
   {
     'type': 'title',
-    'title': 'Friday',
+    'class': 'Friday',
     'content': [
       {
-        'title': 'desu2',
+        'class': 'desu2',
         'content': ['desu2 friday'],
       },
       {
-        'title': 'p5',
+        'class': 'p5',
         'content': ['p5 friday'],
       }
     ],
   },
   {
     'type': 'title',
-    'title': 'Saturday',
+    'class': 'Saturday',
     'content': [
       {
-        'title': 'desu2',
+        'class': 'desu2',
         'content': ['desu2 saturday'],
       },
       {
-        'title': 'p5',
+        'class': 'p5',
         'content': ['p5 saturday'],
       }
     ],
