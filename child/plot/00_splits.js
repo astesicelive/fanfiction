21492d79677aac0ef24a1ec79769ac3373af18b1
leaf <145163317+astesicelive@users.noreply.github.com>
2026-06-21 02:00:11 +0200

    Update 00_splits.js

--- child/plot/00_splits.js
+++ child/plot/00_splits.js
@@ -1,12 +1,12 @@
 const desu2_split = [
   {
-    'type': 'choice';
-    'class_name': 'septentriones';
-    'title': "Maruki manages to drag DeSu2 cast to the future";
+    'type': 'choice',
+    'class_name': 'septentriones',
+    'title': "Maruki manages to drag DeSu2 cast to the future",
   },
   {
-    'type': 'choice';
-    'class_name': 'record_breaker';
-    'title': "Maruki doesn't manage to drag DeSu2 cast to the future";
+    'type': 'choice',
+    'class_name': 'record_breaker',
+    'title': "Maruki doesn't manage to drag DeSu2 cast to the future",
   },
 ];
