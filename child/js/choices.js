7221446c336472c9ca48bdf7a6b892d8e20e25ff
leaf <145163317+astesicelive@users.noreply.github.com>
2026-06-21 20:48:49 +0200

    id hashtag

--- child/js/choices.js
+++ child/js/choices.js
@@ -6,7 +6,7 @@ btn.forEach((b) => {
 function pickChoice() {
   let classes = this.classList;
   let id = $(this).closest('.choice_buttons').attr('id');
-  let section = document.querySelector(id);
+  let section = document.querySelector(`#${id}`);
   let items = section.querySelectorAll('.selected');
   items.forEach((i) => {
     i.classList.toggle('selected');
