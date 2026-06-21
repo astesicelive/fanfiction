a05b97cbfb2a89a7386d8a2ef9bc4ae04ac2058b
leaf <145163317+astesicelive@users.noreply.github.com>
2026-06-21 19:17:12 +0200

    Update choices.js

--- child/js/choices.js
+++ child/js/choices.js
@@ -1,9 +1,13 @@
-const btn = document.querySelectorAll('.button');
-btn.forEach((b) => {
-  b.addEventListener("click", pickChoice};
-});
+function add_choice_button() {
+  const btn = document.querySelectorAll('.button');
+  btn.forEach((b) => {
+    b.addEventListener("click", pickChoice};
+  });
+};
 
 function pickChoice() {
   let classes = this.classList;
   classes.add('selected');
 }
+
+$(document).ready(add_choice_button());
