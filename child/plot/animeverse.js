592ebd2fce7a6135c9c1cc5078a7c9948a0ba86c
leaf <145163317+astesicelive@users.noreply.github.com>
2026-06-25 02:39:52 +0200

    animeverse plot

--- child/plot/animeverse.js
+++ child/plot/animeverse.js
@@ -247,13 +247,13 @@ let animeverse = () => {
   ];
 
   let var_list = [
-    {name: 1},
-    {name: 2},
-    {name: 3},
-    {name: 4},
-    {name: 5},
-    {name: 6},
-    {name: 7}
+    {name: '1'},
+    {name: '2'},
+    {name: '3'},
+    {name: '4'},
+    {name: '5'},
+    {name: '6'},
+    {name: '7'}
   ];
 
   parse_plot(var_list, desu2a_plot, arr);
