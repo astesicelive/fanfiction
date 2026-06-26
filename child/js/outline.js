d7e63e00291c3aacc26ea425c6781616191afabd
leaf <145163317+astesicelive@users.noreply.github.com>
2026-06-26 03:06:50 +0200

    error

--- child/js/outline.js
+++ child/js/outline.js
@@ -4,7 +4,7 @@ outline_container.setAttribute('class', 'outline_container');
 let all_sections = document.querySelectorAll(`.divider, .title`);
 let outline_sections = [];
 all_sections.forEach((cur) => {
-    if (cur.attr('id')) {
+    if (cur.getAttribute('id')) {
         outline_sections.push(cur);
     };
 });
