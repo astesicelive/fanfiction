c5cb83bf5c24be5e0247d859ed8736ce147e579a
leaf <145163317+astesicelive@users.noreply.github.com>
2026-06-28 01:21:39 +0200

    delete pre-txt

---
+++ child/js/plot.js
@@ -0,0 +1,682 @@
+let animeverse = () => { 
+  let arr = [
+    {
+      'type': 'divider',
+      'title': '2011 (animeverse)',
+      'id': 'animeverse',
+    },
+    {
+      'type': 'title',
+      'class_name': 'desu2a sunday',
+      'title': 'Sunday',
+      'content': [
+        {
+          'class_name': 'desu2',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'desu2a monday',
+      'title': 'Monday',
+      'content': [
+        {
+          'class_name': 'desu2',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'desu2a tuesday',
+      'title': 'Tuesday',
+      'content': [
+        {
+          'class_name': 'desu2',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'desu2a wednesday',
+      'title': 'Wednesday',
+      'content': [
+        {
+          'class_name': 'desu2',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'desu2a thursday',
+      'title': 'Thursday',
+      'content': [
+        {
+          'class_name': 'desu2',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'desu2a friday',
+      'title': 'Friday',
+      'content': [
+        {
+          'class_name': 'desu2',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'desu2a saturday',
+      'title': 'Saturday',
+      'content': [
+        {
+          'class_name': 'desu2',
+          'content': [],
+        }
+      ],
+    }
+  ];
+  
+  return arr;
+};
+
+var gameverse = () => {
+  let arr = [
+    {
+      'type': 'divider',
+      'title': '2011 (gameverse)',
+      'id': 'gameverse',
+    },
+    {
+      'type': 'title',
+      'class_name': 'septentriones sunday',
+      'title': 'Sunday (Sep)',
+      'content': [
+        {
+          'class_name': 'desu2 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'p5 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'gameverse_rb',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'septentriones monday',
+      'title': 'Monday (Sep)',
+      'content': [
+        {
+          'class_name': 'desu2 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'p5 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'gameverse_rb',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'septentriones tuesday',
+      'title': 'Tuesday (Sep)',
+      'content': [
+        {
+          'class_name': 'desu2 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'p5 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'gameverse_rb',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'septentriones wednesday',
+      'title': 'Wednesday (Sep)',
+      'content': [
+        {
+          'class_name': 'desu2 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'p5 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'gameverse_rb',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'septentriones thursday',
+      'title': 'Thursday (Sep)',
+      'content': [
+        {
+          'class_name': 'desu2 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'p5 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'gameverse_rb',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'septentriones friday',
+      'title': 'Friday (Sep)',
+      'content': [
+        {
+          'class_name': 'desu2 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'p5 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'gameverse_rb',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'septentriones saturday',
+      'title': 'Saturday (Sep)',
+      'content': [
+        {
+          'class_name': 'desu2 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'p5 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'gameverse_rb',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'triangulum tuesday',
+      'title': 'Doomed Tuesday (Tri)',
+      'content': [
+        {
+          'class_name': 'desu2 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'p5 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'gameverse_rb',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'triangulum wednesday',
+      'title': 'Doomed Wednesday (Tri)',
+      'content': [
+        {
+          'class_name': 'desu2 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'p5 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'gameverse_rb',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'triangulum thursday',
+      'title': 'Doomed Thursday (Tri)',
+      'content': [
+        {
+          'class_name': 'desu2 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'p5 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'gameverse_rb',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'triangulum tuesday',
+      'title': 'Tuesday (Tri)',
+      'content': [
+        {
+          'class_name': 'desu2 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'p5 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'gameverse_rb',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'triangulum wednesday',
+      'title': 'Wednesday (Tri)',
+      'content': [
+        {
+          'class_name': 'desu2 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'p5 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'gameverse_rb',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'triangulum thursday',
+      'title': 'Thursday (Tri)',
+      'content': [
+        {
+          'class_name': 'desu2 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'p5 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'gameverse_rb',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'triangulum friday',
+      'title': 'Friday (Tri)',
+      'content': [
+        {
+          'class_name': 'desu2 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'p5 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'gameverse_rb',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'triangulum saturday',
+      'title': 'Saturday (Tri)',
+      'content': [
+        {
+          'class_name': 'desu2 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'p5 gameverse_child',
+          'content': [],
+        },
+        {
+          'class_name': 'gameverse_rb',
+          'content': [],
+        }
+      ],
+    }
+  ];
+
+  return arr;
+};
+
+var year_2012 = () => {
+  let arr = [
+    {
+      'type': 'divider',
+      'title': '2012',
+      'id': 'year_2012',
+    },
+    {
+      'type': 'title',
+      'class_name': 'post_desu2 aftermath',
+      'title': 'Desu2 Aftermath',
+      'content': [
+        {
+          'class_name': 'aftermath_child',
+          'content': [],
+        },
+        {
+          'class_name': 'aftermath_rb',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'post_desu2 p1',
+      'title': 'P1',
+      'content': [
+        {
+          'class_name': 'p1',
+          'content': [],
+        }
+      ],
+    }
+  ];
+
+  return arr;
+};
+
+var year_2013 = () => {
+  let arr = [
+    {
+      'type': 'divider',
+      'title': '2013',
+      'id': 'year_2013',
+    },
+    {
+      'type': 'title',
+      'class_name': 'post_desu2 aftermath',
+      'title': 'Desu2 Aftermath',
+      'content': [
+        {
+          'class_name': 'aftermath_child',
+          'content': [],
+        }
+      ],
+    }
+  ];
+
+  return arr;
+};
+
+var year_2014 = () => {
+  let arr = [
+    {
+      'type': 'divider',
+      'title': '2014',
+      'id': 'year_2014',
+    },
+    {
+      'type': 'title',
+      'class_name': 'p2_is',
+      'title': 'P2 Innocent Sin',
+      'content': [
+        {
+          'class_name': 'child_innocent_sin',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'p2_ep',
+      'title': 'P2 Eternal Punishment',
+      'content': [
+        {
+          'class_name': 'child_eternal_punishment',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'p3',
+      'title': 'P3 Beginning',
+      'content': [
+        {
+          'class_name': 'p3_past',
+          'content': [],
+        }
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'post_desu2 aftermath',
+      'title': 'Desu2 Aftermath',
+      'content': [
+        {
+          'class_name': 'aftermath_child',
+          'content': [],
+        }
+      ],
+    }
+  ];
+
+  return arr;
+};
+
+var year_2014_2 = () => {
+  let arr = [
+    {
+      'type': 'title',
+      'class_name': 'twin_birthday',
+      'id': 'year_2014_2',
+      'title': 'P5',
+      'content': [
+        {
+          'class_name': 'child_birthday',
+          'content': [],
+        },
+        {
+          'class_name': 'only_akira',
+          'content': [],
+        }
+      ],
+    }
+  ];
+
+  return arr;
+};
+
+var year_2016 = () => {
+  let arr = [
+    {
+      'type': 'divider',
+      'title': '2016',
+      'id': 'year_2016',
+    },
+    {
+      'type': 'title',
+      'class_name': 'fixing_p2',
+      'title': 'Fixing P2',
+      'content': [
+        {
+          'class_name': 'fix_p2',
+          'content': [],
+        }
+      ],
+    }
+  ];
+
+  return arr;
+};
+
+var year_2017 = () => {
+  let arr = [
+    {
+      'type': 'divider',
+      'title': '2017',
+      'id': 'year_2017',
+    },
+    {
+      'type': 'title',
+      'class_name': 'yamahibi_wedding',
+      'title': 'DeSu2 Wedding',
+      'content': [
+        {
+          'class_name': 'ren_nii',
+          'content': [],
+        },
+        {
+          'class_name': 'rin_nii',
+          'content': [],
+        }
+      ],
+    }
+  ];
+
+  return arr;
+};
+
+var year_2018 = () => {
+  let arr = [
+    {
+      'type': 'divider',
+      'title': '2018',
+      'id': 'year_2018',
+    },
+    {
+      'type': 'title',
+      'class_name': 'orphan_naoya',
+      'title': 'DeSu1/2',
+      'content': [
+        {
+          'class_name': 'child',
+          'content': [],
+        },
+        {
+          'class_name': 'rb',
+          'content': [],
+        }
+      ],
+    }
+  ];
+
+  return arr;
+};
+
+var year_2019 = () => {
+  let arr = [
+    {
+      'type': 'divider',
+      'title': '2019',
+      'id': 'year_2019',
+    },
+    {
+      'type': 'title',
+      'class_name': 'juntatsu_wedding',
+      'title': 'P2 Wedding',
+      'content': [
+        {
+          'class_name': 'child',
+          'content': [],
+        },
+      ],
+    }
+  ];
+
+  return arr;
+};
+
+var year_2020 = () => {
+  let arr = [
+    {
+      'type': 'divider',
+      'title': '2020',
+      'id': 'year_2020',
+    },
+    {
+      'type': 'title',
+      'class_name': 'desu1',
+      'title': 'Desu1/2',
+      'content': [
+        {
+          'class_name': 'jungo',
+          'content': [],
+        },
+      ],
+    },
+    {
+      'type': 'title',
+      'class_name': 'yaldy',
+      'title': 'P5',
+      'content': [
+        {
+          'class_name': 'vs_yaldy',
+          'content': [],
+        },
+      ],
+    }
+  ];
+
+  return arr;
+};
+
+var year_2024 = () => {
+  let arr = [
+    {
+      'type': 'divider',
+      'title': '2024',
+      'id': 'year_2024',
+    },
+    {
+      'type': 'title',
+      'class_name': 'p3',
+      'title': 'P3',
+      'content': [
+        {
+          'class_name': 'p3_journey',
+          'content': [],
+        },
+      ],
+    }
+  ];
+
+  return arr;
+};
