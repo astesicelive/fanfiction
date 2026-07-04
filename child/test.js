7cc720f0d3bd8765a63de44cd07eda026845d810
leaf <145163317+astesicelive@users.noreply.github.com>
2026-07-04 19:47:29 +0200

    Update test.js

--- child/test.js
+++ child/test.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-let sheetId_n = '1_4R_CONv5QypZox6tguQ7oQ339-aV5ytq4e4dE-R-l0'
-const sheet_data_n = await ( 
+let sheetId = ${{ secrets.JP_SHEET }}
+const sheet_data = await ( 
   await axios.get(`https://sheets.googleapis.com/v4/spreadsheets/${sheetId}?includeGridData=true&key=AIzaSyA_FFL4muIlo-rOMT1ribmrOJ3_MwDr9o8`)
 ).data;
