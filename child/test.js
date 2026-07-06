a475cf9bd09d085981b3ca0880fef9ecf71ceed7
leaf <145163317+astesicelive@users.noreply.github.com>
2026-07-06 03:54:08 +0200

    Delete child/test.js

--- child/test.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-let sheetId = ${{ secrets.JP_SHEET }};
-let key = ${{ secrets.SHEETS_API }};
-const sheet_data = await ( 
-  await axios.get(`https://sheets.googleapis.com/v4/spreadsheets/${sheetId}?includeGridData=true&key=${key}`)
-).data;
